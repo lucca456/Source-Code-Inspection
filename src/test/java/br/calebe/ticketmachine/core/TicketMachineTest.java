e84530b77f309764039af3a61753c2fb009a9c40
Lucca Guarreschi <luccaguareschi@gmail.com>
2023-11-02 17:14:43 -0300

    structure changes 8

--- src/test/java/br/calebe/ticketmachine/core/TicketMachineTest.java
+++ src/test/java/br/calebe/ticketmachine/core/TicketMachineTest.java
@@ -3,7 +3,6 @@ package br.calebe.ticketmachine.core;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
 class TicketMachineTest {
 
     @Test
@@ -13,32 +12,24 @@ class TicketMachineTest {
     }
 
     @Test
-    void testInserirValido() throws PapelMoedaInvalidaException {
-        TicketMachine machine = new TicketMachine(50);
-        machine.inserir(10);
-        assertEquals(10, machine.getSaldo());
-    }
-
-    @Test
     void testInserirInvalido() {
         TicketMachine machine = new TicketMachine(50);
-        assertThrows(PapelMoedaInvalidaException.class, () -> {
-            machine.inserir(3); // Valor inválido
+        assertThrows(PapelMoedaInvalidaException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                machine.inserir(3); // Valor inválido
+            }
         });
-    }
-
-    @Test
-    void testImprimirSaldoSuficiente() throws SaldoInsuficienteException {
-        TicketMachine machine = new TicketMachine(50);
-        machine.inserir(50);
-        assertNotNull(machine.imprimir());
     }
 
     @Test
     void testImprimirSaldoInsuficiente() {
         TicketMachine machine = new TicketMachine(50);
-        assertThrows(SaldoInsuficienteException.class, () -> {
-            machine.imprimir();
+        assertThrows(SaldoInsuficienteException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                machine.imprimir();
+            }
         });
     }
 }
